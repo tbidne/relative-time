5dc60181372ac3897d15e86c5a4eee7c78708812
Tommy Bidne <tbidne@protonmail.com>
2025-05-13 10:24:06 +1200

    Update haddock script

--- tools/haddock.sh
+++ tools/haddock.sh
@@ -2,12 +2,14 @@ set -e
 
 export LANG="C.UTF-8"
 
-cabal update
 cabal haddock --haddock-hyperlink-source --haddock-quickjump
 
 mkdir -p docs/
 
 # shellcheck disable=SC2038
-find docs/ -type f | xargs -I % sh -c "rm -r %"
+fd -t f . docs | xargs -I % sh -c "rm -r %"
 
-cp -r dist-newstyle/build/x86_64-linux/ghc-9.4.4/relative-time-0.1/doc/html/relative-time/* docs/
+dir=$(fd -t d html dist-newstyle/build)
+
+cp -r $dir/relative-time/* docs/
+
